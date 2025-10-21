3a1adc0533ac8fabe4779ca5ae851690eded22f8
jeromeke <jke006@e.ntu.edu.sg>
2025-10-21 17:51:54 +0800

    updated dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,16 @@
+import 'package:cyclepathsg/login.dart';
 import 'package:cyclepathsg/register.dart';
+import 'package:cyclepathsg/navigation.dart';
+
 import 'package:device_preview/device_preview.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:cyclepathsg/login.dart';
 
 Future<void> main() async {
 
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  // await Firebase.initializeApp();
 
   runApp(DevicePreview(builder: (context)=>MyApp()));
 }
@@ -34,7 +36,8 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       title: 'CyclePathSG',
       debugShowCheckedModeBanner: false,
-      home: RegisterPage(),
+      // home: RegisterPage(),
+      home: NavigationPage(),
     );
   }
 }
